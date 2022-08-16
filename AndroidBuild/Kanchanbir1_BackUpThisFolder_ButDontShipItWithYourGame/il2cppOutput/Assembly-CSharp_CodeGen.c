5d6f0da6611523f3f6b0c0b121ab09a02d7519ee
chamath <hchamath@gmail.com>
2022-08-16 16:22:17 +0530

    kanchanbir1

---
+++ AndroidBuild/Kanchanbir1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,129 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void NavScriptHarshana::LoadScene(System.String)
+extern void NavScriptHarshana_LoadScene_m897F3171C51129F307F129F1FE8E2A0BE486E79F (void);
+// 0x00000002 System.Void NavScriptHarshana::.ctor()
+extern void NavScriptHarshana__ctor_m5BC1A46341AA240102931D618A8BDBEE8E2A6260 (void);
+// 0x00000003 System.Void SpawnInteractable::Start()
+extern void SpawnInteractable_Start_m0326C5279E86D819795C0350684D5D359C908CCB (void);
+// 0x00000004 System.Void SpawnInteractable::ScaleSliderUpdate(System.Single)
+extern void SpawnInteractable_ScaleSliderUpdate_mC260A60C90F989093FF0500EC55B4B4EFCF0C796 (void);
+// 0x00000005 System.Void SpawnInteractable::RotateSliderUpdate(System.Single)
+extern void SpawnInteractable_RotateSliderUpdate_m39C882A7B7C3311D5E5B7069AB7ED851EAF8A21A (void);
+// 0x00000006 System.Void SpawnInteractable::.ctor()
+extern void SpawnInteractable__ctor_mEB92752E986C94DB2062450E34EF24486DB31906 (void);
+// 0x00000007 System.Void SpawnManager::Start()
+extern void SpawnManager_Start_m65C9EA67649948222CFF4FBBF77BE2319D813DAF (void);
+// 0x00000008 System.Void SpawnManager::Update()
+extern void SpawnManager_Update_mD714BA3EADCC182FB7A93B9DE347D727E3A1BC53 (void);
+// 0x00000009 System.Void SpawnManager::SpawnPrefab(UnityEngine.Vector3)
+extern void SpawnManager_SpawnPrefab_m592B5DAAA7685F0012B9B42629E160A253F51D38 (void);
+// 0x0000000A System.Void SpawnManager::.ctor()
+extern void SpawnManager__ctor_m8DD503A0FFE79FA38CF0B7F013E54D24A04D166A (void);
+// 0x0000000B System.Void Hirushka_SpawnableScript::Start()
+extern void Hirushka_SpawnableScript_Start_m1081552CFC9CB4015E4CD315D025313CB9E94B47 (void);
+// 0x0000000C System.Void Hirushka_SpawnableScript::Update()
+extern void Hirushka_SpawnableScript_Update_m4B9E59F1BE770712936CEB5741B3DDAEA322BD67 (void);
+// 0x0000000D System.Void Hirushka_SpawnableScript::SpawnPrefab(UnityEngine.Vector3)
+extern void Hirushka_SpawnableScript_SpawnPrefab_m4A0237E2FEC08FBE6CD7EB91EED0E43AF1B21F51 (void);
+// 0x0000000E System.Void Hirushka_SpawnableScript::.ctor()
+extern void Hirushka_SpawnableScript__ctor_mEFB19258AFD26E63DD4430A427B4C14E431E2D8B (void);
+// 0x0000000F System.Void Hirushka_SpawnInteractable::Start()
+extern void Hirushka_SpawnInteractable_Start_m60D2773228A0D3D5933DF1B87DB84F5991B59B49 (void);
+// 0x00000010 System.Void Hirushka_SpawnInteractable::ScaleSliderUpdate(System.Single)
+extern void Hirushka_SpawnInteractable_ScaleSliderUpdate_m04253D1C7685371CE6650142250D3E6A754D0F54 (void);
+// 0x00000011 System.Void Hirushka_SpawnInteractable::RotateSliderUpdate(System.Single)
+extern void Hirushka_SpawnInteractable_RotateSliderUpdate_mA6C5C17026391A18E0A0FFEB4DEDF3A17605D872 (void);
+// 0x00000012 System.Void Hirushka_SpawnInteractable::.ctor()
+extern void Hirushka_SpawnInteractable__ctor_m5CEA84920BF3B1D01833D18F4251071657D2A0D2 (void);
+// 0x00000013 System.Void Nav_controller_Hiru::LoadScene(System.String)
+extern void Nav_controller_Hiru_LoadScene_mE5F2F7F1993AC5FEB750C5240CD543AECE709A81 (void);
+// 0x00000014 System.Void Nav_controller_Hiru::.ctor()
+extern void Nav_controller_Hiru__ctor_m0286EE5A9E7F5C71815B6A289ACC05BB859C267D (void);
+// 0x00000015 System.Void navscript::LoadScene(System.String)
+extern void navscript_LoadScene_mE69CB9AB340DB15B5D870C831CAA1C0A7BA9B53A (void);
+// 0x00000016 System.Void navscript::.ctor()
+extern void navscript__ctor_m93ED6316B6B8C45AAA905B71835FC51EC5B9C062 (void);
+static Il2CppMethodPointer s_methodPointers[22] = 
+{
+	NavScriptHarshana_LoadScene_m897F3171C51129F307F129F1FE8E2A0BE486E79F,
+	NavScriptHarshana__ctor_m5BC1A46341AA240102931D618A8BDBEE8E2A6260,
+	SpawnInteractable_Start_m0326C5279E86D819795C0350684D5D359C908CCB,
+	SpawnInteractable_ScaleSliderUpdate_mC260A60C90F989093FF0500EC55B4B4EFCF0C796,
+	SpawnInteractable_RotateSliderUpdate_m39C882A7B7C3311D5E5B7069AB7ED851EAF8A21A,
+	SpawnInteractable__ctor_mEB92752E986C94DB2062450E34EF24486DB31906,
+	SpawnManager_Start_m65C9EA67649948222CFF4FBBF77BE2319D813DAF,
+	SpawnManager_Update_mD714BA3EADCC182FB7A93B9DE347D727E3A1BC53,
+	SpawnManager_SpawnPrefab_m592B5DAAA7685F0012B9B42629E160A253F51D38,
+	SpawnManager__ctor_m8DD503A0FFE79FA38CF0B7F013E54D24A04D166A,
+	Hirushka_SpawnableScript_Start_m1081552CFC9CB4015E4CD315D025313CB9E94B47,
+	Hirushka_SpawnableScript_Update_m4B9E59F1BE770712936CEB5741B3DDAEA322BD67,
+	Hirushka_SpawnableScript_SpawnPrefab_m4A0237E2FEC08FBE6CD7EB91EED0E43AF1B21F51,
+	Hirushka_SpawnableScript__ctor_mEFB19258AFD26E63DD4430A427B4C14E431E2D8B,
+	Hirushka_SpawnInteractable_Start_m60D2773228A0D3D5933DF1B87DB84F5991B59B49,
+	Hirushka_SpawnInteractable_ScaleSliderUpdate_m04253D1C7685371CE6650142250D3E6A754D0F54,
+	Hirushka_SpawnInteractable_RotateSliderUpdate_mA6C5C17026391A18E0A0FFEB4DEDF3A17605D872,
+	Hirushka_SpawnInteractable__ctor_m5CEA84920BF3B1D01833D18F4251071657D2A0D2,
+	Nav_controller_Hiru_LoadScene_mE5F2F7F1993AC5FEB750C5240CD543AECE709A81,
+	Nav_controller_Hiru__ctor_m0286EE5A9E7F5C71815B6A289ACC05BB859C267D,
+	navscript_LoadScene_mE69CB9AB340DB15B5D870C831CAA1C0A7BA9B53A,
+	navscript__ctor_m93ED6316B6B8C45AAA905B71835FC51EC5B9C062,
+};
+static const int32_t s_InvokerIndices[22] = 
+{
+	3574,
+	4417,
+	4417,
+	3606,
+	3606,
+	4417,
+	4417,
+	4417,
+	3651,
+	4417,
+	4417,
+	4417,
+	3651,
+	4417,
+	4417,
+	3606,
+	3606,
+	4417,
+	3574,
+	4417,
+	3574,
+	4417,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	22,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};

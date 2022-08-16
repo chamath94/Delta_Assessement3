528b7cac377adf25edae6994ce022329ec66bbdb
Kanchan-1999 <kanchanbir100@gmail.com>
2022-08-16 12:14:51 +0530

    made changes to my scene

---
+++ AndroidBuild/alienrivals_kanchanbir_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,81 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void SpawnManager::Start()
+extern void SpawnManager_Start_m65C9EA67649948222CFF4FBBF77BE2319D813DAF (void);
+// 0x00000002 System.Void SpawnManager::Update()
+extern void SpawnManager_Update_mD714BA3EADCC182FB7A93B9DE347D727E3A1BC53 (void);
+// 0x00000003 System.Void SpawnManager::SpawnPrefab(UnityEngine.Vector3)
+extern void SpawnManager_SpawnPrefab_m592B5DAAA7685F0012B9B42629E160A253F51D38 (void);
+// 0x00000004 System.Void SpawnManager::.ctor()
+extern void SpawnManager__ctor_m8DD503A0FFE79FA38CF0B7F013E54D24A04D166A (void);
+// 0x00000005 System.Void Hirushka_SpawnableScript::Start()
+extern void Hirushka_SpawnableScript_Start_m1081552CFC9CB4015E4CD315D025313CB9E94B47 (void);
+// 0x00000006 System.Void Hirushka_SpawnableScript::Update()
+extern void Hirushka_SpawnableScript_Update_m4B9E59F1BE770712936CEB5741B3DDAEA322BD67 (void);
+// 0x00000007 System.Void Hirushka_SpawnableScript::SpawnPrefab(UnityEngine.Vector3)
+extern void Hirushka_SpawnableScript_SpawnPrefab_m4A0237E2FEC08FBE6CD7EB91EED0E43AF1B21F51 (void);
+// 0x00000008 System.Void Hirushka_SpawnableScript::.ctor()
+extern void Hirushka_SpawnableScript__ctor_mEFB19258AFD26E63DD4430A427B4C14E431E2D8B (void);
+// 0x00000009 System.Void navscript::LoadScene(System.String)
+extern void navscript_LoadScene_mE69CB9AB340DB15B5D870C831CAA1C0A7BA9B53A (void);
+// 0x0000000A System.Void navscript::.ctor()
+extern void navscript__ctor_m93ED6316B6B8C45AAA905B71835FC51EC5B9C062 (void);
+static Il2CppMethodPointer s_methodPointers[10] = 
+{
+	SpawnManager_Start_m65C9EA67649948222CFF4FBBF77BE2319D813DAF,
+	SpawnManager_Update_mD714BA3EADCC182FB7A93B9DE347D727E3A1BC53,
+	SpawnManager_SpawnPrefab_m592B5DAAA7685F0012B9B42629E160A253F51D38,
+	SpawnManager__ctor_m8DD503A0FFE79FA38CF0B7F013E54D24A04D166A,
+	Hirushka_SpawnableScript_Start_m1081552CFC9CB4015E4CD315D025313CB9E94B47,
+	Hirushka_SpawnableScript_Update_m4B9E59F1BE770712936CEB5741B3DDAEA322BD67,
+	Hirushka_SpawnableScript_SpawnPrefab_m4A0237E2FEC08FBE6CD7EB91EED0E43AF1B21F51,
+	Hirushka_SpawnableScript__ctor_mEFB19258AFD26E63DD4430A427B4C14E431E2D8B,
+	navscript_LoadScene_mE69CB9AB340DB15B5D870C831CAA1C0A7BA9B53A,
+	navscript__ctor_m93ED6316B6B8C45AAA905B71835FC51EC5B9C062,
+};
+static const int32_t s_InvokerIndices[10] = 
+{
+	4417,
+	4417,
+	3651,
+	4417,
+	4417,
+	4417,
+	3651,
+	4417,
+	3574,
+	4417,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	10,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
